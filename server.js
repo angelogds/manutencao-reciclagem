4c0ef6ca5e91261ce0b57363359fdb4284cda181
angelogds <126365784+angelogds@users.noreply.github.com>
2025-11-15 23:57:16 -0300

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride("_method"));
 app.use(express.static("public"));
+app.use("/uploads", express.static("uploads"));
 
 // Sessão
 app.use(
